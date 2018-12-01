329fc18731bb909d68fc99d9c8a629bc78e6c4db
fldamiens <florent.damiens@etu.uca.fr>
2018-12-01 20:53:36 +0100

    Webpack jquery

---
+++ assets/js/generic.js
@@ -0,0 +1,8 @@
+var $ = require('jquery');
+window.$ = $;
+window.jQuery = $;
+
+require('../css/Shared/components.css');
+require('../css/Shared/responsee.css');
+
+require('./Shared/responsee.js');
